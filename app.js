78be3e6622852370486cf7c399f22453da972922
Filipa-Freitas <filipafreitascontact@gmail.com>
2021-08-23 17:58:25 +0200

    add multer

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const sauceRoutes = require('./routes/sauce');
 const userRoutes = require('./routes/user');
+const path = require('path');
 
 const app = express();
 
@@ -21,6 +22,7 @@ app.use((req, res, next) => {
     next();
 });
 
+app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use('/api/sauces', sauceRoutes);
 app.use('/api/auth', userRoutes);
 
