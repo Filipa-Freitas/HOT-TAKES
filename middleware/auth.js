a496eeba9ec6e833aa21da2c4e38c582fae07109
Filipa-Freitas <filipafreitascontact@gmail.com>
2021-08-23 17:08:29 +0200

    add server.js, app.js, models, controllers, routes, middleware

---
+++ middleware/auth.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (res, req, next) => {
+    try {
+        const token = req.hearders.authorization.split(' ')[1];
+        const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+        const userId = decodedToken.userId;
+        if (req.body.userId && req.body.userId !== userId) {
+            throw 'userId non valable !';
+        } else {
+            next();
+        }
+    } catch (error) {
+        res.status(401).json({ error: error | 'Requête non authentifiée !' });
+    }
+};
