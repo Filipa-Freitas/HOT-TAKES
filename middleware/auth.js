47dbb049701084bed782d3f557f49687fcc92ed6
Filipa-Freitas <filipafreitascontact@gmail.com>
2021-08-25 17:43:04 +0200

    modify sauce controller, fix auth error, modify model sauce

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 
-module.exports = (res, req, next) => {
+module.exports = (req, res, next) => {
     try {
         const token = req.hearders.authorization.split(' ')[1];
         const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
@@ -11,6 +11,6 @@ module.exports = (res, req, next) => {
             next();
         }
     } catch (error) {
-        res.status(401).json({ error: error | 'Requête non authentifiée !' });
+        res.status(401).json({ error: new Error('Invalid request !') });
     }
 };
